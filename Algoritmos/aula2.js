32921080ceae54c4f8c1a70eef2019e415e51e5d
Raissa <142915471+raissalim@users.noreply.github.com>
2026-02-22 14:25:03 -0300

    Add HTML structure and JavaScript exercises

--- Algoritmos/aula2.js
+++ Algoritmos/aula2.js
@@ -1,1 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicios javaScript</title>
+</head>
+<body>
 
+    <!--Exercicio 1 -->
+      <center>
+        <p>Exercicio 1 </p>
+    
+     <input type="text" id="cidadeNatal"  placeholder="Digite sua cidade natal"><br><br>
+      <input type="text" id="anonasc"  placeholder="Digite o ano de nascimento"><br><br>
+
+       <button type="button"  id= "btnEnviar" onclick="receberCidadeeNasc()">Cadastrar</button><br><br>
+        </center>
+        <br><br>
+
+   <!--Exercicio 2 -->
+         <center>
+            <p>Exercicio 2 </p>
+    
+     <input type="text" id="Peso"  placeholder="Digite seu peso"><br><br>
+      <input type="text" id="Altura"  placeholder="Digite sua altura"><br><br>
+
+       <button type="button"  id= "btnMostrarr" onclick="calculadora()">Fale comigo!</button>
+        </center>
+          <br><br>
+
+  <center>
+   <!--Exercicio 3 -->
+   <p>Exercicio 3 </p>
+    
+     <input type="text" id="nomeTime"  placeholder="Digite nome do seu time"><br><br>
+      <input type="text" id="quanttitulos"  placeholder="Digite  a quantidsde de titulos"><br><br>
+
+       <button type="button"  id= "btnTime" onclick="time()">Amo meu time</button>
+        </center>
+          <br><br>
+
+
+           <center>
+   <!--Exercicio 4 -->
+    <p>Exercicio 4</p>
+    
+     <input type="text" id="nomeRua"  placeholder="Digite nome do logradoouro"><br><br>
+      <input type="text" id="numCasa"  placeholder="Digite o número da rua"><br><br>
+
+       <button type="button"  id= "btnEndereco" onclick="endereco()">Onde eu moro</button>
+        </center>
+          <br><br>
+
+
+
+           <center>
+   <!--Exercicio 5 -->
+   <p>Exercicio 5</p>
+    
+     <input type="text" id="notaEscola"  placeholder="Digite sua nota na escola"><br><br>
+
+       <button type="button"  id= "opiniaoavo" onclick="opinioes()">Opinião vovó</button>
+        <button type="button"  id= "opiniaomae" onclick="opinioes()">Opinião mamae</button>
+
+        </center>
+          <br><br>
+
+
+   <!--Exercício 6-->
+    <p> Exercicio 6<p>
+
+<center>
+<button type="button"   onclick="pare()">Pare!</button>
+
+<button type="button"   onclick="pense()">Pense</button>
+
+<button type="button"   onclick="siga()">Siga</button>
+        </center>
+
+
+
+
+        <script>
+           
+  
+  // exercicio 1
+function receberCidadeeNasc(){
+    var cidade = cidadeNatal.value;
+    var anonascimento = anonasc.value;
+
+    if(cidade.trim() !== ""){
+        alert('Você nasceu na cidade de: ' + cidade + ' em: ' + anonascimento);
+    } else {
+        alert('Por favor digite seus dados novamente');
+    }
+}
+
+// exercicio 2
+function calculadora(){
+    var pesoValor = Peso.value;
+    var alturaValor = Altura.value;
+
+    if(pesoValor.trim() !== ""){
+        alert('Você pesa: ' + pesoValor + ' e sua altura é: ' + alturaValor);
+    } else {
+        alert('Por favor digite seus dados novamente');
+    }
+}
+
+// exercicio 3
+function time(){
+    var nome = nomeTime.value;
+    var titulos = quanttitulos.value;
+
+    if(nome.trim() !== ""){
+        alert('O time: ' + nome + ' tem: ' + titulos + ' títulos');
+    } else {
+        alert('Por favor digite seus dados novamente');
+    }
+}
+
+// exercicio 4
+function endereco(){
+    var rua = nomeRua.value;
+    var numero = numCasa.value;
+
+    if(rua.trim() !== ""){
+        alert('Seu logradouro é: ' + rua + ' e seu número é: ' + numero);
+    } else {
+        alert('Por favor digite seus dados novamente');
+    }
+}
+
+// exercicio 5
+function opiniaoavo(){
+    var notaneto = notaEscola.value;
+          Alert("Muito bem, que neto inteligente, porque tirou " + notaEscola;
+        }
+
+function opiniaomae(){
+  var notaneto = notaEscola.value;
+          Alert("Não fez mais que tua obrigação com essa nota " + notaEscola;)
+        }
+
+   
+    Function pare(){
+Alert("pare e aguarde ");
+  
+}
+Function pense(){
+Alert("Pense se pode seguir ou se deve parar!");
+    }
+Function siga(){
+Alert("Pode seguir adiante!");
+    }
+
+
+
+
+
+
+</script>
+    
+</body>
+</html>
